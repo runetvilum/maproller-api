c05aa7fc756e3f159db46b6019d114f317254e1d
Rune Tvilum <rune@addin.dk>
2014-11-13 08:02:39 +0100

    cors

--- index.js
+++ index.js
@@ -45,18 +45,25 @@ app.use('/mbtiles', function (req, res) {
         req.pipe(request.get(url)).pipe(res);
     }
 });
-app.all('/couchdb*', function (req, res) {
-
+app.all('/couchdb*',  function (req, res) {
+    res.set('Access-Control-Allow-Credentials', 'true');
+    res.set('Access-Control-Allow-Origin', 'http://localhost:8100');
+    res.set('Access-Control-Allow-Methods','GET, PUT, POST, HEAD, DELETE');
+    res.set('Access-Control-Allow-Headers','accept, authorization, content-type, origin, referer');
     var url = "http://localhost:5984/" + req.url.substring(9);
     console.log(url);
     //console.log(inspect(req,{colors:true}));
-
+    //var url = 'http://admin:rutv2327@localhost:5984/admin/' + req.params.id + '/logo.png';
     if (req.method === 'PUT') {
         req.pipe(request.put(url)).pipe(res);
     } else if (req.method === 'POST') {
         req.pipe(request.post(url)).pipe(res);
     } else if (req.method === 'GET') {
         req.pipe(request.get(url)).pipe(res);
+    } else if (req.method === 'DELETE') {
+        req.pipe(request.del(url)).pipe(res);
+    } else if (req.method === 'OPTIONS') {
+        res.end();
     }
 
 });
