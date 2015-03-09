397e4f2f847d1bb9c372ca3b0296e0f07e653b0f
Rune Tvilum <rune@addin.dk>
2015-03-09 17:01:32 +0100

    attachments

--- upgrade.js
+++ upgrade.js
@@ -14,6 +14,68 @@ var argv = require('minimist')(process.argv.slice(2)),
             roles: []
         }
     };
+
+function insert(doc, configuration, secdoc, dbOrganization, organization) {
+    dbOrganization.insert(doc, configuration.id, function (err, doc) {
+        if (!err) {
+            console.log("insert organization configuration: " + configuration.id);
+        } else {
+            console.log("error insert organization configuration: " + configuration.id);
+        }
+        nano.db.create('db-' + configuration.id, function (err, body) {
+            if (!err) {
+                console.log('create db-' + configuration.id);
+            } else {
+                console.log('error create db-' + configuration.id);
+            }
+            var dbConfiguration = nano.db.use('db-' + configuration.id);
+
+            dbConfiguration.insert(rolesdoc, "_security", function (err, body) {
+                dbConfiguration.insert(secdoc, '_design/security', function (err, body) {
+                    if (err) {
+                        console.log("error validate_doc_update configuration " + organization.id);
+                    }
+                    dbConfiguration.get(configuration.id, function (err, configdoc) {
+                        var doc = {
+                            hidden: configuration.doc.hidden || false,
+                            map: configuration.doc.map,
+                            widgets: configuration.doc.widgets
+                        };
+                        if (doc.widgets) {
+                            for (var i = 0; i < doc.widgets.length; i++) {
+                                if (doc.widgets[i].id === 'indberetninger') {
+                                    doc.widgets[i].description = configuration.doc.instruction;
+                                    break;
+                                }
+                            }
+                        }
+                        if (!err) {
+                            doc._rev = configdoc._rev;
+                        }
+                        if (configuration.doc._attachments) {
+                            doc._attachments = {};
+                            for (var key in configuration.doc._attachments) {
+                                if (key.indexOf('.geojson')) {
+                                    doc._attachments[key] = {
+                                        content_type: configuration.doc._attachments[key].content_type,
+                                        data: configuration.doc._attachments[key].data
+                                    };
+                                }
+                            }
+                        }
+                        dbConfiguration.insert(doc, configuration.id, function (err, body) {
+                            if (err) {
+                                console.log("error insert doc: " + configuration.id);
+                            } else {
+                                console.log("insert doc: " + configuration.id);
+                            }
+                        });
+                    });
+                });
+            });
+        });
+    });
+}
 if (argv.config) {
     config = jf.readFileSync(argv.config);
     nano = require('nano')({
@@ -63,74 +125,20 @@ if (argv.config) {
                                             doc._rev = configdoc._rev;
                                         }
                                         if (configuration.doc._attachments && configuration.doc._attachments.logo) {
-                                            
-                                            doc._attachments = {
-                                                logo: {
-                                                    content_type: configuration.doc._attachments.logo.content_type,
-                                                    data: configuration.doc._attachments.logo.data
-                                                }
-                                            };
-                                        }
-                                        console.log(doc);
-                                        dbOrganization.insert(doc, configuration.id, function (err, doc) {
-                                            if (!err) {
-                                                console.log("insert organization configuration: " + configuration.id);
-                                            } else {
-                                                console.log("error insert organization configuration: " + configuration.id);
-                                            }
-                                            nano.db.create('db-' + configuration.id, function (err, body) {
-                                                if (!err) {
-                                                    console.log('create db-' + configuration.id);
-                                                } else {
-                                                    console.log('error create db-' + configuration.id);
-                                                }
-                                                var dbConfiguration = nano.db.use('db-' + configuration.id);
-
-                                                dbConfiguration.insert(rolesdoc, "_security", function (err, body) {
-                                                    dbConfiguration.insert(secdoc, '_design/security', function (err, body) {
-                                                        if (err) {
-                                                            console.log("error validate_doc_update configuration " + organization.id);
-                                                        }
-                                                        dbConfiguration.get(configuration.id, function (err, configdoc) {
-                                                            var doc = {
-                                                                hidden: configuration.doc.hidden || false,
-                                                                map: configuration.doc.map,
-                                                                widgets: configuration.doc.widgets
-                                                            };
-                                                            if (doc.widgets) {
-                                                                for (var i = 0; i < doc.widgets.length; i++) {
-                                                                    if (doc.widgets[i].id === 'indberetninger') {
-                                                                        doc.widgets[i].description = configuration.doc.instruction;
-                                                                        break;
-                                                                    }
-                                                                }
-                                                            }
-                                                            if (!err) {
-                                                                doc._rev = configdoc._rev;
-                                                            }
-                                                            if (configuration.doc._attachments) {
-                                                                doc._attachments = {};
-                                                                for (var key in configuration.doc._attachments) {
-                                                                    if (key.indexOf('.geojson')) {
-                                                                        doc._attachments[key] = {
-                                                                            content_type: configuration.doc._attachments[key].content_type,
-                                                                            data: configuration.doc._attachments[key].data
-                                                                        };
-                                                                    }
-                                                                }
-                                                            }
-                                                            dbConfiguration.insert(doc, configuration.id, function (err, body) {
-                                                                if (err) {
-                                                                    console.log("error insert doc: " + configuration.id);
-                                                                } else {
-                                                                    console.log("insert doc: " + configuration.id);
-                                                                }
-                                                            });
-                                                        });
-                                                    });
-                                                });
+                                            dbOrganization.attachment.get(configuration.id, 'logo', function (err, data) {
+                                                doc._attachments = {
+                                                    logo: {
+                                                        content_type: configuration.doc._attachments.logo.content_type,
+                                                        data: data
+                                                    }
+                                                };
+                                                insert(doc, configuration, secdoc, organization);
                                             });
-                                        });
+
+                                        } else {
+                                            insert(doc, configuration, secdoc, dbOrganization, organization);
+                                        }
+
                                     });
                                 });
                             });
