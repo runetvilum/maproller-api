0fe4be2d226364e4b8237be8d8b6d67de050f7a4
Rune Tvilum <rune@addin.dk>
2015-03-19 14:07:27 +0100

    email

--- index2.js
+++ index2.js
@@ -98,53 +98,45 @@ var inspect = require('util').inspect,
                     }
                     db.get(change.id, function (err, doc) {
                         db_admin.view('database', 'emailtemplate', emailoptions, function (err, data) {
-                            data.rows.forEach(function (row) {
-                                var key, ok, item, email;
-                                if (row.doc.users) {
-                                    for (key in row.doc.users) {
-                                        if (row.doc.users.hasOwnProperty(key)) {
-                                            item = row.doc.users[key];
-                                            ok = testrules(item.rules, doc);
-                                            if (ok) {
-                                                template(row.id, {
-                                                    doc: doc
-                                                }, sendmail(key, row));
+                            if (!err) {
+                                data.rows.forEach(function (row) {
+                                    var key, ok, item, email;
+                                    if (row.doc.users) {
+                                        for (key in row.doc.users) {
+                                            if (row.doc.users.hasOwnProperty(key)) {
+                                                item = row.doc.users[key];
+                                                ok = testrules(item.rules, doc);
+                                                if (ok) {
+                                                    template(row.id, {
+                                                        doc: doc
+                                                    }, sendmail(key, row));
+                                                }
                                             }
                                         }
                                     }
-                                }
-                                if (row.doc.userfields) {
-                                    for (key in row.doc.userfields) {
-                                        if (row.doc.userfields.hasOwnProperty(key)) {
-                                            email = valuepath(key, doc);
-                                            item = row.doc.userfields[key];
-                                            ok = testrules(item.rules, doc);
-                                            if (ok && email) {
-                                                template(row.id, {
-                                                    doc: doc
-                                                }, sendmail(email, doc));
+                                    if (row.doc.userfields) {
+                                        for (key in row.doc.userfields) {
+                                            if (row.doc.userfields.hasOwnProperty(key)) {
+                                                email = valuepath(key, doc);
+                                                item = row.doc.userfields[key];
+                                                ok = testrules(item.rules, doc);
+                                                if (ok && email) {
+                                                    template(row.id, {
+                                                        doc: doc
+                                                    }, sendmail(email, doc));
+                                                }
                                             }
                                         }
                                     }
-                                }
-                            });
-                            db.get('_local/follow_since', function (err, doc) {
-                                /*console.log(inspect(doc, {
-                                                colors: true
-                                            }));*/
-                                doc = doc || {};
-                                doc.since = change.seq;
-                                db.insert(doc, '_local/follow_since', function (err, body) {
-                                    /*if (!err) {
-                                                    console.log(inspect(body, {
-                                                        colors: true
-                                                    }));
-                                                } else {
-                                                    console.log("insert error: " + err);
-                                                }*/
-                                    feed.resume();
                                 });
-                            });
+                                db.get('_local/follow_since', function (err, doc) {
+                                    doc = doc || {};
+                                    doc.since = change.seq;
+                                    db.insert(doc, '_local/follow_since', function (err, body) {
+                                        feed.resume();
+                                    });
+                                });
+                            }
                         });
                     });
                 }
